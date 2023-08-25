22de810b14e2669fee45bdb32739fadc8d0931f8
qnasir <qnasir575@gmail.com>
2023-08-26 00:34:54 +0530

    result

---
+++ app.js
@@ -0,0 +1,8 @@
+var buyBtn = document.getElementById("buy")
+
+var text = document.getElementById("thanks-text")
+
+buyBtn.onclick=()=>{
+    text.style.display="block"
+    buyBtn.style.display="none"
+}
